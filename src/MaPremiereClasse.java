3050b82080ce27148beafdcd73b95ec1002a61a1
Wafae DOUMAL <wafae@MacBook-Pro-de-Wafae.local>
2017-10-17 16:01:25 +0200

    un msg

---
+++ src/MaPremiereClasse.java
@@ -0,0 +1,21 @@
+
+public class MaPremiereClasse {
+	
+	public static void main(String[] args) {
+		
+		String msg1 = "Bonjour";
+		String msg2 = " à tous";
+		
+		String[] msg  = {msg1, msg2};
+		//for(int i = 0 ; i < msg.length ; i++)
+		for (String var : msg)
+		{
+		    
+			//System.out.print(msg[i]);
+		    System.out.print(var);
+		}
+		
+	}
+}
+	
+
