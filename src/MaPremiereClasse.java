55bf43ec88c1f0bf66385cbd310c4b8d5e9083b4
Wafae DOUMAL <wafae@MacBook-Pro-de-Wafae.local>
2017-10-17 17:24:51 +0200

    un msg3

--- src/MaPremiereClasse.java
+++ src/MaPremiereClasse.java
@@ -8,13 +8,16 @@ public class MaPremiereClasse {
 		int age = 18;
 		
 		String[] msg  = {msg1, msg2};
-		//for(int i = 0 ; i < msg.length ; i++)
+		for(int i = 0 ; i < msg.length ; i++)
 		//for (String var : msg)
-		//{
-		    
-			System.out.print(msg[0]+ age + msg[1]);
+		{
+		if( i== 0)
+			System.out.print(msg1);
+		else
+			System.out.print(age + msg2 );
+			//System.out.print(msg[0]+ age + msg[1]);
 		    //System.out.print(var);
-		//}
+		}
 		
 	}
 }
